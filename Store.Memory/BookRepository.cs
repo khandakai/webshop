d4580c298d57cf8c175e6deb9d578ebdfc50bfa7
Handakai <stepanntosh@gmail.com>
2023-01-08 02:38:21 +0800

    Create web-app

--- Store.Memory/BookRepository.cs
+++ Store.Memory/BookRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace Store.Memory;
+﻿using System.Linq;
+namespace Store.Memory;
 public class BookRepository : IBookRepository
 {
+    private readonly Book[] books = new[]
+    {
+        new Book(1, "Art of Programming"),
+        new Book(2, "Refactoring"),
+        new Book(3, "C programming language")
+    };
 
+    public Book[] GetAllByTitle(string titlePart)
+    {
+        return books.Where(book => book.Title.Contains(titlePart))
+            .ToArray();
+    }
 }
 
