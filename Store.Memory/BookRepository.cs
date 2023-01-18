f7a3bfa22909e42fea0e2226b5b283f8cd13b334
khandakai <magaabduraxman@gmail.com>
2023-01-18 22:44:11 +0700

    Implement search by ISBN and Author

--- Store.Memory/BookRepository.cs
+++ Store.Memory/BookRepository.cs
@@ -4,14 +4,22 @@ public class BookRepository : IBookRepository
 {
     private readonly Book[] books = new[]
     {
-        new Book(1, "Art of Programming"),
-        new Book(2, "Refactoring"),
-        new Book(3, "C programming language")
+        new Book(1, "Art of Programming", "ISBN 12312-31231", "D. Knuth"),
+        new Book(2, "Refactoring", "ISBN 12312-31232", "M. Fowler"),
+        new Book(3, "C programming language", "ISBN 12312-31233", "B. Kernighan, D. Ritchie")
     };
 
-    public Book[] GetAllByTitle(string titlePart)
+    public Book[] GetAllByIsbn(string isbn)
     {
-        return books.Where(book => book.Title.Contains(titlePart))
+        return books.Where(book => book.Isbn == isbn)
+            .ToArray();
+    }   
+
+
+    public Book[] GetAllByTitleOrAuthor(string titlePart)
+    {
+        return books.Where(book => book.Author.Contains(titlePart)
+                                || book.Title.Contains(titlePart))
             .ToArray();
     }
 }
--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,13 +1,32 @@
-﻿namespace Store;
+﻿using System.Text.RegularExpressions;
+
+namespace Store;
 public class Book
 {
     public int Id { get; }
 
     public string Title { get; }
 
-    public Book(int id, string title)
+    public string Isbn { get; }
+
+    public string Author { get; }
+
+    public Book(int id, string title, string isbn, string author)
     {
         Id = id;
         Title = title;
+        Isbn = isbn;
+        Author = author;
+    }
+
+    internal static bool IsIsbn(string? isbn)
+    {
+        if (isbn is null) return false;
+
+        isbn = isbn
+            .Replace("-", "")
+            .Replace(" ", "")
+            .ToUpper();
+        return Regex.IsMatch(isbn, @"^ISBN\d{10}(\d{3})?$");
     }
 }
